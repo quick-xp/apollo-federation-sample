79c487722e8bf1479789e858f5c17eaf905f8b4f
r.nishio <quick.xp@gmail.com>
2019-08-25 21:56:20 +0900

    dev

--- gateway/src/infrastructure/server.ts
+++ gateway/src/infrastructure/server.ts
@@ -1,13 +1,25 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import { ApolloServer } from 'apollo-server'
+import { ApolloGateway } from '@apollo/gateway'
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.listen(3000, () => {
-  console.log('listening on port 3000')
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: 'accounts', url: 'http://localhost:3001/graphql' },
+    { name: 'products', url: 'http://localhost:3002/graphql' },
+    { name: 'reviews', url: 'http://localhost:3003/graphql' }
+  ]
+})
+
+const server = new ApolloServer({ gateway })
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
 })
 
 export default app

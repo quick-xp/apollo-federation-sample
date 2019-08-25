e262c5fdbfe5149bbd77f6d5f3efcc8bda45a851
r.nishio <quick.xp@gmail.com>
2019-08-25 18:07:37 +0900

    dev

--- account-ruby-2.3/db/seeds.rb
+++ account-ruby-2.3/db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  [
+    {
+      name: 'Ada Lovelace',
+      username: '@ada',
+      email: 'ada@dummy.test',
+    },
+    {
+      name: 'Alan Turing',
+      username: '@complete',
+      email: 'alan@dummy.test',
+    },
+    {
+      name: 'Taro Yamada',
+      username: '@yamada',
+      email: 'yamada@dummy.test',
+    },
+    {
+      name: 'Sato Tanaka',
+      username: '@tanaka',
+      email: 'tanaka@dummy.test',
+    }
+  ]
+)

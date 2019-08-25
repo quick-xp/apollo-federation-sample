c85ed133264c15809f8cfec3fafebe0abf0ae6f8
r.nishio <quick.xp@gmail.com>
2019-08-25 17:40:36 +0900

    create user model

--- account-ruby-2.3/db/schema.rb
+++ account-ruby-2.3/db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190825084009) do
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

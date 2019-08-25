6b8db54e5b55956072465e772f8316ceda91d528
r.nishio <quick.xp@gmail.com>
2019-08-25 18:46:49 +0900

    annotate

--- product-ruby-2.3/spec/models/product_spec.rb
+++ product-ruby-2.3/spec/models/product_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :bigint           not null, primary key
+#  upc        :string(255)
+#  name       :string(255)
+#  price      :integer
+#  weight     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do

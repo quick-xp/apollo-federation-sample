5e9a706038e5400ac23f3c05f2a0a5f3b1089f6a
r.nishio <quick.xp@gmail.com>
2019-08-25 21:04:18 +0900

    dev

--- product-ruby-2.3/app/graphql/product_ruby23_schema.rb
+++ product-ruby-2.3/app/graphql/product_ruby23_schema.rb
@@ -1,4 +1,5 @@
 class ProductRuby23Schema < GraphQL::Schema
+  include ApolloFederation::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 end
--- review-ruby-2.3/app/graphql/review_ruby23_schema.rb
+++ review-ruby-2.3/app/graphql/review_ruby23_schema.rb
@@ -1,4 +1,5 @@
 class ReviewRuby23Schema < GraphQL::Schema
+  include ApolloFederation::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 end

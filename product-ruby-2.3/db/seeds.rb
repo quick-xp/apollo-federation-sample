726faab7b480f86b8f71ccfaab31d8588f88f94f
r.nishio <quick.xp@gmail.com>
2019-08-25 18:57:50 +0900

    seeds

--- product-ruby-2.3/db/seeds.rb
+++ product-ruby-2.3/db/seeds.rb
@@ -1,6 +1,28 @@
 Product.create!(
   [
     {
-    }
+      upc: '100000010011',
+      name: 'iron sword',
+      price: 20000,
+      weight: 20,
+    },
+    {
+      upc: '100000010021',
+      name: 'wooden sword',
+      price: 1000,
+      weight: 10,
+    },
+    {
+      upc: '100000020011',
+      name: 'potion',
+      price: 100,
+      weight: 1,
+    },
+    {
+      upc: '100000020021',
+      name: 'high portion',
+      price: 1000,
+      weight: 1,
+    },
   ]
 )

ff327bf34b20181dee94ed9cd7916f63d9459a66
r.nishio <quick.xp@gmail.com>
2019-08-25 22:15:09 +0900

    dev

--- gateway/src/infrastructure/server.ts
+++ gateway/src/infrastructure/server.ts
@@ -1,32 +1,48 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import { ApolloServer } from 'apollo-server'
-import { ApolloGateway } from '@apollo/gateway'
+import express, { Request } from 'express'
+import * as bodyParser from 'body-parser'
+import { ApolloServer, gql } from 'apollo-server-express'
+import { ApolloGateway, RemoteGraphQLDataSource } from '@apollo/gateway'
+const { setContext } = require('apollo-link-context')
+import { createHttpLink } from 'apollo-link-http'
+import { GraphQLFormattedError, GraphQLError } from 'graphql'
 
-const app = express()
-const env = app.get('env')
-const currentPath = process.cwd()
+async function run() {
+  const app = express()
 
-if (env === 'development') {
-  const dotenv = require('dotenv')
-  dotenv.config({ path: `${currentPath}/config/${env}.env` })
+  // server
+  const env = app.get('env')
+  const currentPath = process.cwd()
+
+  if (env === 'development') {
+    const dotenv = require('dotenv')
+    dotenv.config({ path: `${currentPath}/config/${env}.env` })
+  }
+  app.set('port', process.env.PORT || 3000)
+
+  const gateway = new ApolloGateway({
+    serviceList: [
+      { name: 'accounts', url: `${process.env.ACCOUNT_URL}/graphql` },
+      { name: 'products', url: `${process.env.PRODUCT_URL}/graphql` },
+      { name: 'reviews', url: `${process.env.REVIEW_URL}/graphql` }
+    ]
+  })
+
+  const server = new ApolloServer({
+    gateway: gateway,
+    introspection: true,
+    subscriptions: false
+  })
+
+  server.applyMiddleware({ app: app })
+
+  const port = app.get('port')
+  app.listen(port)
+
+  if (env === 'development') {
+    console.info(
+      `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+    )
+  }
 }
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-const gateway = new ApolloGateway({
-  serviceList: [
-    { name: 'accounts', url: `${process.env.ACCOUNT_URL}/graphql` },
-    { name: 'products', url: `${process.env.PRODUCT_URL}/graphql` },
-    { name: 'reviews', url: `${process.env.REVIEW_URL}/graphql` }
-  ]
-})
-
-const server = new ApolloServer({ gateway })
-
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`)
-})
-
-export default app
+run()

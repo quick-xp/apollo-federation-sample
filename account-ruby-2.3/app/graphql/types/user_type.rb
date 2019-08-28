c261e5080e43da0c76fce8a91f95cf4b9ea6db8b
r.nishio <quick.xp@gmail.com>
2019-08-28 18:09:22 +0900

    dev

--- account-ruby-2.3/app/graphql/types/user_type.rb
+++ account-ruby-2.3/app/graphql/types/user_type.rb
@@ -1,8 +1,14 @@
 module Types
   class UserType < Types::BaseObject
+    key fields: 'id'
+
     field :id, ID, null: false
     field :name, String, null: true
     field :username, String, null: true
     field :email, String, null: true
+
+    def self.resolve_reference(object, _context)
+      User.find(object[:id])
+    end
   end
 end
